d1923f4c3f8b98539bc58cc1549796b64e82bb9c
babbu000 <111335500+babbu47@users.noreply.github.com>
2022-08-30 07:00:28 +0000

    update text

---
+++ index.js
@@ -0,0 +1,102 @@
+readlineSync = require("readline-sync");
+var playername = readlineSync.question("whats your name? ");
+console.log("--------")
+console.log(playername + " lets play a quiz");
+console.log("-------")
+console.log("rules:-")
+console.log("1)each question  is of 1 mark")
+
+console.log("-----")
+
+var score = 0;
+//var highscore=[{name:"rahul",score:5},{name:"ankit",score:4}]
+console.log("Q1 which fruit is my favourite");
+option = ["mango", "appple", "banana"];
+index = readlineSync.keyInSelect(option, "ans")
+
+if (option[index] == option[0]) {
+  score++
+  console.log("you are right")
+  console.log("keep going you score is - " + score);
+  ;
+}
+else {
+  
+  console.log("you are wrong");
+ 
+}
+console.log("------")
+console.log("Q2 which bike among i like the most? ");
+option1 = ["pulsar 220", "ktmrc 390", "duke 390"];
+index1 = readlineSync.keyInSelect(option1, "ans")
+
+if (option1[index1] == option1[2]) {
+  score++;
+  console.log("you are right");
+  console.log("")
+}
+else {
+  
+  console.log("you are wrong");
+  
+}
+console.log("-------")
+console.log("Q3 Which game i was more addicted to  ? ");
+option2 = ["pubg ", "clash of clans", "free fire"];
+index2 = readlineSync.keyInSelect(option2, "ans")
+
+if (option2[index2] == option2[2]) {
+  score++;
+  console.log("you are right");
+  console.log("")
+}
+else {
+  
+  console.log("you are wrong");
+  
+}
+console.log("-------")
+console.log("Q4 Which is my first watched anime ? ");
+option3 = ["death note", "AOT", "Demon slayer"];
+index3 = readlineSync.keyInSelect(option3, "ans")
+
+if (option3[index3] == option3[2]) {
+  score++;
+  console.log("good job");
+  console.log("")
+}
+else {
+  
+  console.log(" wrong");
+  
+}
+console.log("-------")
+console.log("Q5 who is my favourite captain? ");
+option4 = ["MS dhoni ", "virat  kholi", "rohit sharma"];
+index4 = readlineSync.keyInSelect(option4, "ans")
+
+if (option4[index4] == option4[0]) {
+  score++;
+  console.log("bdiya bhai");
+
+}
+else {
+  
+  console.log(" you are wrong!");
+
+}
+console.log("-------");
+console.log("-------")
+console.log("your score = " + score + "/5")
+console.log("-------");
+console.log("------");
+console.log("Thank You!");
+
+
+
+
+
+
+
+
+
